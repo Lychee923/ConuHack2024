e0ed694dda48d97f77bace8c40b047f14d829070
Lychee923 <129560516+Lychee923@users.noreply.github.com>
2024-01-20 17:36:47 -0500

    added change stance in Players

--- Player.py
+++ Player.py
@@ -12,14 +12,17 @@ class Player:
     def move(self, direction):
         self.y += direction
 
-    #Walking stance
+    #Update stance
+    def change_stance(self):
+        self.walk_stance += 1
+
+    #Return walking stance
     def get_stance(self):
         if self.walk_stance % 2 == 0:
             stance = " o  \nn+--\n />  "
         else:
             stance = " o  \nn+--\n >\\  "
 
-        self.walk_stance += 1    
         return stance
 
 
