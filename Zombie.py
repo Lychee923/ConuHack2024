7424037fe20bb74effdc29ddb20d96ad0cf7e101
Jie Zan <jie.zan@mail.mcgill.ca>
2024-01-20 18:03:17 -0500

    Merge branch 'main' of https://github.com/Lychee923/ConuHack2024

--- Zombie.py
+++ Zombie.py
@@ -1,26 +1,26 @@
+
 class Zombie:
-    
     head = "_<"
     legs1 = "<\\"
-    
+
     legs2 = "/<"
 
-    def __init__(self, x, y, hp=3, speed=2):
+    def __init__(self, x, y, hp=3, speed=1):
         self.hp = hp
         self.speed = speed
         self.x = x
         self.y = y
         self.alive = True
-        
+
     def move(self, direction):
-        if direction.equals("LEFT"):
+        if direction == "LEFT":
             self.x -= self.speed
-        if direction.equals("UP"):
+        if direction == "UP":
             self.y += 1
-        if direction.equals("DOWN"):
+        if direction == "DOWN":
             self.y -= 1
-    
-    def take_damage(self, damage =1):
+
+    def take_damage(self, damage=1):
         self.hp -= damage
         if self.hp <= 0:
             self.alive = False
