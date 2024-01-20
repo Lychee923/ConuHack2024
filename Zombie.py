0197b24f74cc2ccd732a62ca038673f3598b1455
Justin <justin.mao@mcgill.ca>
2024-01-20 16:43:48 -0500

    zombies syntax format

--- Zombie.py
+++ Zombie.py
@@ -4,34 +4,25 @@ class Zombie:
     legs1 = "<\\"
     
     legs2 = "/<"
-    
-    
-    def __init__(self, x,y, hp=3, speed =2):
-        self.hp =hp
+
+    def __init__(self, x, y, hp=3, speed=2):
+        self.hp = hp
         self.speed = speed
         self.x = x
         self.y = y
         self.alive = True
         
     def move(self, direction):
-        if(direction.equals("LEFT")):
+        if direction.equals("LEFT"):
             self.x -= self.speed
-        if(direction.equals("UP")):
+        if direction.equals("UP"):
             self.y += 1
-        if(direction.equals("DOWN")):
+        if direction.equals("DOWN"):
             self.y -= 1
     
     def take_damage(self, damage =1):
         self.hp -= damage
-        if (self.hp<=0):
+        if self.hp <= 0:
             self.alive = False
-        if(self.hp == 2):
-            self.speed +=1
-            
-    
-    
-            
-            
-        
-        
-        
+        if self.hp == 2:
+            self.speed += 1
